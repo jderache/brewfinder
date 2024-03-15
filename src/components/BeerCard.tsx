eefec239487836dab9025a7c39b3b485360dd672
Julien DERACHE <julienderache.pro@gmail.com>
2024-03-15 21:17:01 +0100

    :sparkles: add header & footer on flatlist BeerScreen

--- src/components/BeerCard.tsx
+++ src/components/BeerCard.tsx
@@ -10,16 +10,14 @@ interface BeerCardProps {
 
 const BeerCard = ({ product_name, generic_name, image_url}: BeerCardProps) => {
   return (
-    <View className='flex p-4'>
-      <Card className='bg-white'>
+      <Card className='bg-white p-2 m-3'>
         <Card.Cover source={{ uri: image_url }} resizeMode="contain" className='w-full h-[250px] flex-col flex-1 mx-auto bg-slate-50'/>
         <Card.Content className='p-4'>
           <Title className='font-bold text-xl'>{product_name}</Title>
           <Paragraph>{generic_name}</Paragraph>
-          <Button mode='contained' className='mt-2' loading={true}>En savoir plus</Button>
+          <Button mode='contained' className='mt-2' onPress={() => console.log("En savoir plus")} loading={true}>En savoir plus</Button>
         </Card.Content>
       </Card>
-    </View>
   );
 }
 
--- src/screens/BeersScreen.tsx
+++ src/screens/BeersScreen.tsx
@@ -1,29 +1,44 @@
 import React from "react";
 import { SafeAreaView, Text, View, Image, FlatList} from "react-native"
-import { Appbar, ActivityIndicator} from "react-native-paper";
+import { Appbar, ActivityIndicator, Button} from "react-native-paper";
 import { useBeers } from "../hooks/useBeers";
 import BeerCard from "../components/BeerCard";
 
+function ListHeaderComponent() {
+  return (
+    <Text className="text-center text-2xl font-bold p-4">French Beers</Text>
+  );
+}
+
 export default function BeersScreen() {
-  const { data, isLoading, isError, error } = useBeers({ categories_tags: "french%20beers", page_size: 10 });
+  const { data, isLoading, isError, error } = useBeers({ categories_tags: "french%20beers", page_size: 9});
 
   return (
     <>
           <Appbar.Header className="bg-white">
             <Appbar.Content title="BrewFinder 🍺" />
           </Appbar.Header>
-          <SafeAreaView className="flex-1">
+          <SafeAreaView className="flex-1 ">
             <View className="flex-1">
             {isError && !isLoading ? <Text>An error has occurred : {error.message}</Text> : null}
             {isLoading && <Text>Loading...<ActivityIndicator animating={true} /></Text>}
             {data && (
-              <FlatList className="p-5 flex-1"
-                data={data}
-                keyExtractor={(item) => item.product_name}
-                renderItem={({ item }) => (
-                  <BeerCard {...item}/>
-                )}
-              />
+                <FlatList className="p-4 flex-1"
+                  ListHeaderComponent={ListHeaderComponent}
+                  data={data}
+                  keyExtractor={(item) => item.product_name}
+                  renderItem={({ item }) => (
+                    <BeerCard {...item}/>
+                    )}
+                  ListFooterComponent={
+                    <Button
+                      mode="outlined"
+                      onPress={() => console.log("Load more")}
+                      className="m-4 mb-5 font-bold bg-white">
+                      Load more
+                    </Button>
+                  }
+                />
             )}
             </View>
           </SafeAreaView>
